cf295b9b70dc47d27249264af6da335896db7e4a
Dom Ioime <ddioimejr@gmail.com>
2022-06-15 08:29:03 -0400

    making assignment 3 look better

--- Dom_Ioime/Code/Python/Assignment3.py
+++ Dom_Ioime/Code/Python/Assignment3.py
@@ -16,9 +16,6 @@ HIREYEAR = "HIREYEAR"
 DEPT = "DEPT"
 OFFICE = "OFFICE"
 COURSES = "COURSES"
-STUDENT_ATTRIBUTES = "(ID, NAME, SURNAME, GRADYEAR, MAJOR, EMAIL)"
-INSTRUCTOR_ATTRIBUTES = "(ID, NAME, SURNAME, TITLE, HIREYEAR, DEPT, EMAIL)"
-ADMIN_ATTRIBUTES = "(ID, NAME, SURNAME, TITLE, OFFICE, EMAIL)"
 
 
 def create_connection(db_file):
@@ -33,6 +30,7 @@ def create_connection(db_file):
 def create_table(conn, table, attribute):
     cur = conn.cursor()
     try:
+        print("CREATE TABLE {} ({})".format(table, attribute))
         cur.execute("CREATE TABLE {} ({})".format(table, attribute))
     except Error as e:
         print(e)
@@ -40,13 +38,15 @@ def create_table(conn, table, attribute):
 def remove_table(conn, table):
     cur = conn.cursor()
     try:
-        cur.execute("DROP TABLE " + table)
+        print("DROP TABLE {}".format(table))
+        cur.execute("DROP TABLE {}".format(table))
     except Error as e:
         print(e)
 
 def print_table(conn, table):
     cur = conn.cursor()
     try:
+        print("SELECT * FROM {}".format(table))
         cur.execute("SELECT * FROM {}".format(table))
         print(cur.fetchall())
     except Error as e:
@@ -55,7 +55,8 @@ def print_table(conn, table):
 def search_table(conn, table, attribute, value):
     cur = conn.cursor()
     try:
-        cur.execute("SELECT * FROM " + table + " WHERE " + attribute + " = ?", (value,))
+        print("SELECT * FROM {} WHERE {} = '{}'".format(table, attribute, value))
+        cur.execute("SELECT * FROM {} WHERE {} = '{}'".format(table, attribute, value))
         data = cur.fetchall()
         return data
     except Error as e:
@@ -64,21 +65,24 @@ def search_table(conn, table, attribute, value):
 def insert_row(conn, table, attributes, values):
     cur = conn.cursor()
     try:
-        cur.execute("INSERT INTO {} ({}) VALUES {}".format(table, attributes, values))
+        print("INSERT INTO {} {} VALUES {}".format(table, attributes, values))
+        cur.execute("INSERT INTO {} {} VALUES {}".format(table, attributes, values))
     except Error as e:
         print(e)
 
 def remove_row(conn, table, attribute, value):
     cur = conn.cursor()
     try:
-        cur.execute("DELETE FROM " + table + " WHERE " + attribute + "=" + value)
+        print("DELETE FROM {} WHERE {} = {}".format(table, attribute, value))
+        cur.execute("DELETE FROM {} WHERE {} = {}".format(table, attribute, value))
     except Error as e:
         print(e)
 
-def update_value(conn, table, attribute, old_value, new_value):
+def update_value(conn, table, id, id_value, attribute, new_value):
     cur = conn.cursor()
     try:
-        cur.execute("UPDATE " + table + " SET " + attribute + " = ? WHERE " + attribute + " = ?", (new_value, old_value))
+        print("UPDATE {} SET {} = '{}' WHERE {} = '{}'".format(table, attribute, new_value, id, id_value))
+        cur.execute("UPDATE {} SET {} = '{}' WHERE {} = '{}'".format(table, attribute, new_value, id, id_value))
         data = cur.fetchall()
         return data
     except Error as e:
@@ -88,6 +92,7 @@ def get_table_names(conn):
     list = []
     cur = conn.cursor()
     try:
+        print("SELECT name FROM sqlite_master WHERE type='table';")
         cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
         tables = cur.fetchall()
         for i in tables:
@@ -96,34 +101,44 @@ def get_table_names(conn):
     except Error as e:
         print(e)
 
-def get_attribute_names(conn, table):
+def get_table_info(conn, table):
     list = []
     cur = conn.cursor()
     try:
         cur.execute("PRAGMA table_info({})".format(table))
         info = cur.fetchall()
         for i in info:
-            list.append(i[1])
+            list.append(i)
         return list
     except Error as e:
         print(e)
 
-#def find_matching_instructor(conn, course_table, instructor_table, )
+def find_matching_instructors(conn):
+    cur = conn.cursor()
+    print("SELECT INSTRUCTOR.NAME, INSTRUCTOR.SURNAME, COURSES.TITLE FROM INSTRUCTOR INNER JOIN COURSES ON INSTRUCTOR.DEPT = COURSES.DEPT")
+    cur.execute("SELECT INSTRUCTOR.NAME, INSTRUCTOR.SURNAME, COURSES.TITLE FROM INSTRUCTOR INNER JOIN COURSES ON INSTRUCTOR.DEPT = COURSES.DEPT")
+    fetch = cur.fetchall()
+    print(fetch)
 
 def main():
-    # create a database connection
-    conn = create_connection('./Dom_Ioime/Code/Python/assignment3.db')
+    # # create a database connection
+    conn = create_connection('./Dom_Ioime/Code/Python/assignment3.db') 
 
-    #show current student table
+    # #show current student table
     print_table(conn, STUDENT)
 
-    #add 2 students
-    ######################################FIGURE OUT####################################################
-    insert_row(conn, STUDENT, str(get_attribute_names(conn, STUDENT)), "555555, 'Tom', 'Reggy', 2026, 'BSEE', 'reggyt'")
-    insert_row(conn, STUDENT, "(ID, NAME, SURNAME, GRADYEAR, MAJOR, EMAIL)", "(397674, 'Dom', 'Ioime', 2023, 'BSCO', 'ioimed')")
+    # #add 2 students
+    student_attributes = []
+    for i in get_table_info(conn, STUDENT):
+        student_attributes.append(i[1])
+
+    insert_row(conn, STUDENT, tuple(student_attributes), "(555555, 'Tom', 'Reggy', 2026, 'BSEE', 'reggyt')")
+    insert_row(conn, STUDENT, tuple(student_attributes), "(397674, 'Dom', 'Ioime', 2023, 'BSCO', 'ioimed')")
 
     #show updated student table
     print_table(conn, STUDENT)
+
+    print("------------------------------------------------------------------------------------------------------------\n")
 
     #show Instructor table
     print_table(conn, INSTRUCTOR)
@@ -134,45 +149,76 @@ def main():
     #show Instructor table
     print_table(conn, INSTRUCTOR)
 
+    print("------------------------------------------------------------------------------------------------------------\n")
+
     #show Admin table
     print_table(conn, ADMIN)
 
     #update admin value
-    update_value(conn, ADMIN, TITLE, "Registrar", "Vice-President")
+    update_value(conn, ADMIN, ID, 30002, TITLE, "Vice-President")
 
     #show Admin table
     print_table(conn, ADMIN)
+
+    print("------------------------------------------------------------------------------------------------------------\n")
 
     #print initial table names
     print(get_table_names(conn))
 
     #create a new course table
-    create_table(conn, "COURSES", "CRN text, TITLE text, DEPT text, TIME text, DAYS_OF_WEEK text, SEMESTER text, YEAR int, CREDITS int")
+    create_table(conn, "COURSES", "CRN int primary key, TITLE text, DEPT text, TIME text, DAYS_OF_WEEK text, SEMESTER text, YEAR int, CREDITS int")
 
     #check to see if it was created with correct attributes
     print(get_table_names(conn))
-    print(get_attribute_names(conn, COURSES))
+    print(get_table_info(conn, COURSES))
 
-    # #populate with 5 courses
-    insert_row(conn, COURSES, "(CRN, TITLE, DEPT, TIME, DAYS_OF_WEEK, SEMESTER, YEAR, CREDITS)", "('33180', 'Computer Networks for Engineers', 'BSCO', '8:00 - 9:20', 'TR', 'Summer', 2022, 4)")
-    insert_row(conn, COURSES, "(CRN, TITLE, DEPT, TIME, DAYS_OF_WEEK, SEMESTER, YEAR, CREDITS)", "('33169', 'Advanced Digital Circuit Design', 'BSCO', '8:00 - 9:20', 'WF', 'Summer', 2022, 4)")
-    insert_row(conn, COURSES, "(CRN, TITLE, DEPT, TIME, DAYS_OF_WEEK, SEMESTER, YEAR, CREDITS)", "('33185', 'Signals and Systems', 'BSCO', '10:00 - 11:50', 'MW', 'Summer', 2022, 4)")
-    insert_row(conn, COURSES, "(CRN, TITLE, DEPT, TIME, DAYS_OF_WEEK, SEMESTER, YEAR, CREDITS)", "('33175', 'Applied Programming Concepts', 'BSCO', '10:00 - 10:50', 'T', 'Summer', 2022, 3)")
-    insert_row(conn, COURSES, "(CRN, TITLE, DEPT, TIME, DAYS_OF_WEEK, SEMESTER, YEAR, CREDITS)", "('33203', 'Embedded Systems', 'BSCO', '5:00 - 6:20', 'MW', 'Summer', 2022, 3)")
+    print("------------------------------------------------------------------------------------------------------------\n")
 
-    # #print updated table
+    courses_attributes = []
+    for i in get_table_info(conn, COURSES):
+        courses_attributes.append(i[1])
+
+    #populate with 5 courses
+    insert_row(conn, COURSES, tuple(courses_attributes), "(33180, 'Computer Networks for Engineers', 'BSCO', '8:00 - 9:20', 'TR', 'Summer', 2022, 4)")
+    insert_row(conn, COURSES, tuple(courses_attributes), "(33169, 'Advanced Digital Circuit Design', 'BSCO', '8:00 - 9:20', 'WF', 'Summer', 2022, 4)")
+    insert_row(conn, COURSES, tuple(courses_attributes), "(33185, 'Signals and Systems', 'BSCO', '10:00 - 11:50', 'MW', 'Summer', 2022, 4)")
+    insert_row(conn, COURSES, tuple(courses_attributes), "(33175, 'Applied Programming Concepts', 'BSCO', '10:00 - 10:50', 'T', 'Summer', 2022, 3)")
+    insert_row(conn, COURSES, tuple(courses_attributes), "(33203, 'Embedded Systems', 'BSCO', '5:00 - 6:20', 'MW', 'Summer', 2022, 3)")
+
+    #print updated table
     print_table(conn, COURSES)
 
-    print("-----------------------------")
+    print("------------------------------------------------------------------------------------------------------------\n")
 
-    #search 
-    search_table(conn, COURSES, "(DAYS_OF_WEEK)", "MW")
+    #Random Querys on courses DB
+    print(search_table(conn, COURSES, "DAYS_OF_WEEK", 'MW'))
+    update_value(conn, COURSES, "CRN", "33203", DEPT, "HUSS")
+    
+    #print updated table
+    print_table(conn, COURSES)
 
+    print("------------------------------------------------------------------------------------------------------------\n")
 
-    conn.commit()
+    #find matching instructors for the courses
+    find_matching_instructors(conn)
+
+    #ending for db modification
+    #conn.commit()
     conn.close()
-
-
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+##for inserting courses using menu option
+    # response = []
+    # for i in courses_attributes:
+    #     answer = input(i + "?: ")
+    #     response.append(answer)
+    #
+    #tuple(response) <-- goes into query
